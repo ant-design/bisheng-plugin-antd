f6e881244bada7a7b683da00083a2f78e9437df9
jiang <155259966@qq.com>
2016-07-20 09:28:14 +0800

    fix: video should loop (#1)

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -8,7 +8,7 @@ export default function VideoPlayer({ video }) {
   return (
     <div className={`preview-image-box ${videoClassName}`}>
       <div className={'preview-image-wrapper'}>
-        <SublimeVideo src={src} type="video/mp4" />
+        <SublimeVideo src={src} type="video/mp4" loop/>
       </div>
       <div className="preview-image-title">{alt}</div>
       <div className="preview-image-description"
