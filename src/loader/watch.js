c3e7d1e25bc48eb107b4d6bc0c0e70b7aaac5e4f
Benjy Cui <benjytrys@gmail.com>
2016-11-02 17:31:17 +0800

    fix: preview should work

--- src/loader/watch.js
+++ src/loader/watch.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const ts = require('typescript');
+const generator = require('babel-generator').default;
+const transformer = require('bisheng-plugin-react/lib/transformer');
 const utils = require('../utils');
 
 module.exports = function watch(tsCode) {
@@ -18,9 +20,15 @@ module.exports = function watch(tsCode) {
     es6: Prism.highlight(es6Code, Prism.languages.jsx),
     ts: Prism.highlight(tsCode, Prism.languages.typescript),
   };
-  const preview = utils.getPreview(es6Code);
-  return 'module.exports = {\n' +
+  const preprocessedCode = JSON.stringify(utils.getPreview(es6Code));
+  const res = transformer(preprocessedCode, '__react');
+  const preview = generator(res.inputAst, null, preprocessedCode).code;
+  res.inputAst.program.body = res.imports;
+  const imports = generator(res.inputAst, null, preprocessedCode).code;
+
+  return `${imports}\n` +
+    'module.exports = {\n' +
     `  highlightedCode: ${JSON.stringify(highlightedCode)},\n` +
-    `  preview: ${JSON.stringify(preview)}` +
+    `  preview: ${preview}` +
     '\n}';
 }
