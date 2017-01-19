f085dfebba4abba431cc386fd5434843a9889aad
Benjy Cui <benjytrys@gmail.com>
2017-01-19 16:01:57 +0800

    fix: should work with latest bisheng-plugin-react

--- src/loader/watch.js
+++ src/loader/watch.js
@@ -20,14 +20,8 @@ module.exports = function watch(tsCode) {
     es6: Prism.highlight(es6Code, Prism.languages.jsx),
     ts: Prism.highlight(tsCode, Prism.languages.typescript),
   };
-  const preprocessedCode = JSON.stringify(utils.getPreview(es6Code));
-  const res = transformer(preprocessedCode, '__react');
-  const preview = generator(res.inputAst, null, preprocessedCode).code;
-  res.inputAst.program.body = res.imports;
-  const imports = generator(res.inputAst, null, preprocessedCode).code;
-
-  return `${imports}\n` +
-    'module.exports = {\n' +
+  const preview = transformer(es6Code);
+  return 'module.exports = {\n' +
     `  highlightedCode: ${JSON.stringify(highlightedCode)},\n` +
     `  preview: ${preview.replace(/;$/, '')}` +
     '\n}';
